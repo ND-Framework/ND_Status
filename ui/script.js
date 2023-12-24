57896efc2fcabe7145250ca5abc782daa5c20bcb
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2023-12-24 03:27:57 +0100

    feat: show/hide reversed values

--- ui/script.js
+++ ui/script.js
@@ -9,7 +9,7 @@ const actions = {
     add: function(item) {
         const info = item.info;
         statusUI[info.type] = "hidden"
-        $(".info").append(`
+        $(".info").prepend(`
             <status-circle id="circle-${info.type}" style="display: none;"
                 circle-color="${info?.style?.circleColor || "white"}"
                 icon-color="${info?.style?.iconColor || "white"}"
@@ -26,12 +26,12 @@ const actions = {
                 let newValue2 = value.status/newValue;
                 const element = $(`#circle-${value.type}`);
                 element.attr("value", newValue2);
-                console.log(newValue2, statusUI[value.type], JSON.stringify(value))
     
-                if (newValue2 > 90 && statusUI[value.type] == "shown") {
+                const reversed = value.reversed
+                if ((!reversed && newValue2 > 90 || reversed && newValue2 < 10) && statusUI[value.type] == "shown") {
                     statusUI[value.type] = "hidden";
                     element.fadeOut();
-                } else if (newValue2 < 90 && statusUI[value.type] == "hidden") {
+                } else if ((!reversed && newValue2 < 90 || reversed && newValue2 > 10) && statusUI[value.type] == "hidden") {
                     statusUI[value.type] = "shown";
                     element.fadeIn();
                 };
